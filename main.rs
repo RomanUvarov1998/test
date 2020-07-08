7e04bda5633491553460cee98cfbf87e72730928
Roman Uvarov <uvarovrs40@gmail.com>
2020-07-08 22:01:41 +0300

    new_commit

--- main.rs
+++ main.rs
@@ -1,3 +1,34 @@
 fn main() {
 	println!("hello");
 }
+fn foo() {
+
+}
+dsfdsf
+sdf
+sd
+f
+dsf
+ds
+fs
+df
+sd
+f
+sdf
+sd
+f
+fffffffffffffsdff
+
+sd
+f
+dsf
+sdf
+
+
+sdf
+sdf
+sd
+f
+
+111111111
+2222222222
---
+++ mod2.rs
@@ -0,0 +1,3 @@
+pub fn f1(a: u8, b: u8) -> u8 {
+	a + b
+} 
